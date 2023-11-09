bb8b2ab68b8496fc5ba315d80a83a8b75aa447d5
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-11-09 20:50:28 +0200

    09.11

--- src/2.ts
+++ src/2.ts
@@ -16,24 +16,19 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, private department: string, protected salary: number) {
   }
-
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 1000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
 
-export {};
+export { };
